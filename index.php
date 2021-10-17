f674da19ce237074ac2f906a96736a03190afe29
CoalPlays <coalcrafter202@gmail.com>
2021-10-17 22:23:53 +0200

    restaurants werden jetzt dynamisch angezeigt. datenbank für restaurant mit restaurant und food tablle erstellt.

--- index.php
+++ index.php
@@ -3,89 +3,6 @@
 // Sessionhandling starten
 session_start();
 
-// Datenbankverbindung
-include('include/dbconnector.inc.php');
-
-$error = '';
-$message = '';
-$username = $password = '';
-
-
-// Formular wurde gesendet und Besucher ist noch nicht angemeldet.
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // username
-    if (isset($_POST['username'])) {
-        //trim and sanitize
-        $username = htmlspecialchars(trim($_POST['username']));
-
-        // Prüfung username
-        if (empty($username) || !preg_match("/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{6,30}/", $username)) {
-            $error .= "Der Benutzername entspricht nicht dem geforderten Format.<br />";
-        }
-    } else {
-        $error .= "Geben Sie bitte den Benutzername an.<br />";
-    }
-    // password
-    if (isset($_POST['password'])) {
-        //trim and sanitize
-        $password = trim($_POST['password']);
-        // passwort gültig?
-        if (empty($password) || !preg_match("/(?=^.{8,255}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/", $password)) {
-            $error .= "Das Passwort entspricht nicht dem geforderten Format.<br />";
-        }
-    } else {
-        $error .= "Geben Sie bitte das Passwort an.<br />";
-    }
-
-    // kein Fehler
-    if (empty($error)) {
-        // Query erstellen
-        $query = "SELECT id, username, password from users where username = ?";
-
-        // Query vorbereiten
-        $stmt = $mysqli->prepare($query);
-        if ($stmt === false) {
-            $error .= 'prepare() failed ' . $mysqli->error . '<br />';
-        }
-        // Parameter an Query binden
-        if (!$stmt->bind_param("s", $username)) {
-            $error .= 'bind_param() failed ' . $mysqli->error . '<br />';
-        }
-        // Query ausführen
-        if (!$stmt->execute()) {
-            $error .= 'execute() failed ' . $mysqli->error . '<br />';
-        }
-        // Daten auslesen
-        $result = $stmt->get_result();
-
-        // Userdaten lesen
-        if ($row = $result->fetch_assoc()) {
-
-            // Passwort ok?
-            if (password_verify($password, $row['password'])) {
-
-                // Session personifizieren
-                $_SESSION['username'] = $username;
-                $_SESSION['loggedin'] = true;
-
-                // Session ID regenerieren
-                $_SESSION['userid'] = session_regenerate_id(true);
-
-                // weiterleiten auf admin.php
-                header("location: admin.php");
-
-                // Script beenden
-                die();
-            } else {
-                $error .= "Benutzername oder Passwort sind falsch";
-            }
-        } else {
-            $error .= "Benutzername oder Passwort sind falsch";
-        }
-    }
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -98,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/aa92474866.js" crossorigin="anonymous"></script>
 </head>
@@ -112,16 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto ml-auto">
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </ul>
-        </div>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav ms-auto">
                 <?php
                 //wenn Session personalisiert
                 if (isset($_SESSION['loggedin'])) {
@@ -153,57 +61,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="container">
 
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="230" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <?php
+
+                include('include/dbconnectorRestaurants.inc.php');
+
+                $query = "SELECT * FROM restaurants";
+
+                $stmt = $mysqli->prepare($query);
+
+                $stmt->execute();
+
+                $result = $stmt->get_result();
+
+
+                foreach ($result as $value) {
+
+                    echo "<div class='col'>
+                    <div class='card shadow-sm'>
+                        <svg class='bd-placeholder-img card-img-top' width='100%' height='230' xmlns='http://www.w3.org/2000/svg' role='img' preserveAspectRatio='xMidYMid slice' focusable='false'>
                             <title>Placeholder</title>
-                            <image href="images/mc.jpg" height="100%" width="100%" />
+                            <image href='images/mc.jpg' height='100%' width='100%' />
                         </svg>
 
-                        <div class="card-body">
-                            <p class="card-text">McDonald's®</p>
-                            <p class="card-text">Burgers • American • Fast Food<br>Centralbahnstrasse 9, 4051</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <a href="restaurant.php" class="stretched-link">View</a>
-                                <small class="text-muted">15-25 Min</small>
+                        <div class='card-body'>
+                            <p class='card-text'>", $value['name'], "</p>
+                            <p class='card-text'>", $value["description"], "<br>", $value["place"], "</p>
+                            <div class='d-flex justify-content-between align-items-center'>
+                            <a onclick='goTo(", $value["id"], ")' href='' class='stretched-link' >View</a>
+                            <small class='text-muted'>", $value["delivery-from"], "-", $value["delivery-until"], " Min</small>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="230" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <image href="images/subway.jpeg" height="100%" width="100%" />
-                        </svg>
+                </div>";
+                }
 
-                        <div class="card-body">
-                            <p class="card-text">Subway®</p>
-                            <p class="card-text">Sandwich • American<br>Centralbahnpl. 6, 4051</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <a href="restaurant.php" class="stretched-link">View</a>
-                                <small class="text-muted">30-40 Min</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="230" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <image href="images/king.jpeg" height="100%" width="100%" />
-                        </svg>
 
-                        <div class="card-body">
-                            <p class="card-text">Burger King</p>
-                            <p class="card-text">American • Fast Food • Burgers<br>Steinenvorstadt 51, 4051</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <a href="restaurant.php" class="stretched-link">View</a>
-                                <small class="text-muted">35-45 Min</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                ?>
+
+                <a href="" onclick="goTo(4)">resr</a>
 
             </div>
 
@@ -213,5 +108,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+<script>
+    function goTo(id) {
+        var myform = document.createElement("form");
+    myform.action = "restaurant.php";
+    myform.method = "post";
+
+
+    product = document.createElement("input");
+    product.value = id;
+    product.name = "restaurantID";
+
+    myform.appendChild(product);
+    myform.submit();
+    }
+</script>
 
 </html>
