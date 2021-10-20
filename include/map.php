11885e9d704d755074552a996104774e1774afc7
CoalPlays <coalcrafter202@gmail.com>
2021-10-20 22:21:31 +0200

    orders page is now dynamic and not logged in users cant connect

--- include/map.php
+++ include/map.php
@@ -7,14 +7,17 @@
   <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 
     <!--Content-->
-    <div class="modal-content">
+    <div onseeking="getMap()" class="modal-content">
 
       <!--Body-->
       <div class="modal-body mb-0 p-0">
 
         <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-          <iframe src="https://maps.google.com/maps?q=höldeliweg22&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
-            style="border:0" allowfullscreen></iframe>
+          <?php 
+          echo "<iframe id='map' src='https://maps.google.com/maps?q={$test}&t=&z=13&ie=UTF8&iwloc=&output=embed' frameborder='0'
+            style='border:0' allowfullscreen></iframe>"
+          ?>
+          
         </div>
 
       </div>
@@ -34,7 +37,3 @@
 <!--Modal: Name-->
 
 
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
