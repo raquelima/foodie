7fdf4aba0335e9a34ffa639a667f8682e52e8396
CoalPlays <coalcrafter202@gmail.com>
2021-10-25 21:52:20 +0200

    added target blank on social media links and added facebook link

--- include/footer.php
+++ include/footer.php
@@ -21,13 +21,13 @@
         </a>
 
         <ul class="nav col-md-4 justify-content-end">
-            <li class="ms-3"><a class="text-muted" href="https://twitter.com/FoodieLima"><svg class="bi" width="24" height="24">
+            <li class="ms-3"><a class="text-muted" target="blank" href="https://twitter.com/Foodie_yum"><svg class="bi" width="24" height="24">
                         <use xlink:href="#twitter" />
                     </svg></a></li>
-            <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/foodie_yumyumyum/"><svg class="bi" width="24" height="24">
+            <li class="ms-3"><a class="text-muted" target="blank" href="https://www.instagram.com/foodie_yumyumyum/"><svg class="bi" width="24" height="24">
                         <use xlink:href="#instagram" />
                     </svg></a></li>
-            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
+            <li class="ms-3"><a class="text-muted" target="blank" href="https://www.facebook.com/Foodie-107459701732785"><svg class="bi" width="24" height="24">
                         <use xlink:href="#facebook" />
                     </svg></a></li>
         </ul>
